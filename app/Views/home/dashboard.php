0ac19cf860c0ae0aa1c0575a3896be0d073c5f14
bayci <bayci@BaycikNoteL>
2021-08-27 12:11:17 +0300

    working on user system

---
+++ app/Views/home/dashboard.php
@@ -0,0 +1,9 @@
+
+<div style="display: grid;grid-template-columns:1fr 7fr">
+    <div>
+        <a href="User/register_form" target="screen">register</a><br>
+        <a href="User/login_form" target="screen">login</a>
+        
+    </div>
+    <iframe name="screen" style="width:100%;height: 700px"></iframe>
+</div>
