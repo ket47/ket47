329fb2c3d25797fdb647ff07239c3d571d0c0853
bayci <bayci@BaycikNoteL>
2021-09-16 15:53:39 +0300

    working on tezkel

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -56,7 +56,7 @@ class BaseController extends Controller
 		//--------------------------------------------------------------------
 		$this->session = \Config\Services::session();
                 header('Access-Control-Allow-Origin: *');
-                header("Access-Control-Allow-Headers: *");
+                header("Access-Control-Allow-Headers: Origin,X-Requested-With,Content-Type,Accept,Access-Control-Request-Method,Authorization,Cache-Control");
                 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 	}
         
