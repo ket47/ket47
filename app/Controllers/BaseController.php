bb047f0b639d9472c1153245ad639f207494bf95
bayci <bayci@BaycikNoteL>
2021-09-27 17:01:55 +0300

    working on tezkel

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -80,7 +80,7 @@ class BaseController extends Controller
                     break;
                 }
             }
-            header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+            header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, x-sid");
             header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
             header("Access-Control-Allow-Credentials: true");
             header("Access-Control-Expose-Headers: x-sid");
