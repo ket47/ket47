a9e24d8399d466a8debee744c9f423d62a72b9db
bayci <bayci@BaycikNoteL>
2021-10-22 14:24:05 +0300

    working on tezkel

--- db_update/2021-10-22-1318.sql
+++ db_update/2021-10-22-1318.sql
@@ -1,11 +1,8 @@
 ALTER TABLE `product_group_member_list` 
-ADD COLUMN `created_at` DATETIME NULL AFTER `group_id`,
-ADD COLUMN `updated_at` DATETIME NULL AFTER `created_at`;
+ADD COLUMN `created_at` DATETIME NULL AFTER `group_id`;
 
 ALTER TABLE `store_group_member_list` 
-ADD COLUMN `created_at` DATETIME NULL AFTER `group_id`,
-ADD COLUMN `updated_at` DATETIME NULL AFTER `created_at`;
+ADD COLUMN `created_at` DATETIME NULL AFTER `group_id`;
 
 ALTER TABLE `user_group_member_list` 
-ADD COLUMN `created_at` DATETIME NULL AFTER `group_id`,
-ADD COLUMN `updated_at` DATETIME NULL AFTER `created_at`;
+ADD COLUMN `created_at` DATETIME NULL AFTER `group_id`;
