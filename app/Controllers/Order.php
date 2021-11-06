faf661c079e6ce8ccd7120c781620a0d525a9202
bayci <bayci@BaycikNoteL>
2021-11-06 14:08:05 +0300

    working on tezkel

--- app/Controllers/Order.php
+++ app/Controllers/Order.php
@@ -22,14 +22,14 @@ class Order extends \App\Controllers\BaseController {
     }
 
     public function itemCreate() {
-        $order_order_id = $this->request->getVar('order_order_id');
+        $order_store_id = $this->request->getVar('order_store_id');
         $entry_list_json = $this->request->getVar('entry_list');
         $entry_list = [];
         if ($entry_list_json) {
             $entry_list = json_decode($entry_list_json);
         }
         $OrderModel = model('OrderModel');
-        $result = $OrderModel->itemCreate($order_order_id, $entry_list);
+        $result = $OrderModel->itemCreate($order_store_id, $entry_list);
         if ($result === 'forbidden') {
             return $this->failForbidden($result);
         }
