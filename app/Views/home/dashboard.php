17673b2b170064f36237deb03f9ca2493ca8a6a5
bayci <bayci@BaycikNoteL>
2022-01-02 14:12:51 +0300

    bugfix

--- app/Views/home/dashboard.php
+++ app/Views/home/dashboard.php
@@ -4,10 +4,10 @@
             <a href="javascript:scr.location.reload()">RELOAD</a><br><br>
             <hr>
             <h3>Admin</h3>
-            <a href="Admin/Permission/" target="scr">Permissions</a><br>
-            <a href="Admin/GroupManager/" target="scr">Group Manager</a><br>
-            <a href="Admin/TaskManager/" target="scr">Task Manager</a><br>
-            <a href="Admin/PrefManager/" target="scr">Preferences</a><br>
+            <a href="Admin/Permission" target="scr">Permissions</a><br>
+            <a href="Admin/GroupManager" target="scr">Group Manager</a><br>
+            <a href="Admin/TaskManager" target="scr">Task Manager</a><br>
+            <a href="Admin/PrefManager" target="scr">Preferences</a><br>
             <h3>Orders</h3>
             <a href="Home/order_manager" target="scr">Orders</a><br>
             <h3>Products</h3>
@@ -16,7 +16,7 @@
             <h3>Stores</h3>
             <a href="Home/store_manager" target="scr">Stores</a><br>
             <h3>Users</h3>
-            <a href="Home/courier_manager/" target="scr">Couriers</a><br>
+            <a href="Home/courier_manager" target="scr">Couriers</a><br>
             <a href="Home/user_manager" target="scr">Users</a><br>
             <a href="Home/user_register_form" target="scr">Sign Up</a><br>
             <a href="Home/user_login_form" target="scr">Sign In</a><br>
