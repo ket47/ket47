8573c5b71e9471457a37362d5eedf88268eb33f5
bayci <bayci@BaycikNoteL>
2022-01-14 14:46:19 +0300

    response code

--- public/image/get.php
+++ public/image/get.php
@@ -23,6 +23,11 @@ $content_type='';
 $file_optimised=$images_path.'optimised/'.$uri_filename;
 if ( !file_exists($file_optimised) ) {
     $file_source=$images_path.$hash.'.webp';
+    if(!file_exists($file_source)){
+        http_response_code(410);
+        die;
+    }
+
     $thumb= image_resize($file_source, $width, $height);
     
     switch( $extension ){
