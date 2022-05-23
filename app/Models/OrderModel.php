30c32eaacf8409114313b8187becbeaae907ac3b
bayci <bayci@BaycikNoteL>
2022-05-23 10:19:43 +0300

    bugfix

--- app/Models/OrderModel.php
+++ app/Models/OrderModel.php
@@ -83,7 +83,7 @@ class OrderModel extends Model{
         $StoreModel=model('StoreModel');
         $UserModel=model('UserModel');
         $OrderGroupMemberModel->orderBy('order_group_member_list.created_at DESC,link_id DESC');
-        $StoreModel->select('store_id,store_name,store_phone');
+        //$StoreModel->select('store_id,store_name,store_phone');
         $UserModel->select('user_id,user_name,user_phone');
         $order->stage_next=  $this->itemGetNextStages($order->stage_current,$order->user_role);
         $order->stages=     $OrderGroupMemberModel->memberOfGroupsListGet($order->order_id);
