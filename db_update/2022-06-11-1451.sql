f22d777c39f1a8de0bdb120a7094164f68db28b2
bayci <bayci@BaycikNoteL>
2022-06-17 17:31:13 +0300

    working on push

--- db_update/2022-06-11-1451.sql
+++ db_update/2022-06-11-1451.sql
@@ -5,6 +5,6 @@ CREATE TABLE `message_sub_list` (
   `sub_device` VARCHAR(200) NULL,
   `sub_type` VARCHAR(45) NULL,
   `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`sub_id`)
+  PRIMARY KEY (`sub_id`),
   UNIQUE KEY `registrunique` (`sub_registration_id`)
   )ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;;
