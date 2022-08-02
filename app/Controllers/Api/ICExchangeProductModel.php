fabd8f7921c19ef6fed3e247956705791706b186
bayci <bayci@BaycikNoteL>
2022-08-02 15:31:56 +0300

    bugfix

--- app/Controllers/Api/ICExchangeProductModel.php
+++ app/Controllers/Api/ICExchangeProductModel.php
@@ -55,8 +55,8 @@ class ICExchangeProductModel extends Model{
         $attributes=$this->productAttributesParse($xml_product);
         $updated_product=(object)[
             'product_code'=>(string) $xml_product->Артикул?$xml_product->Артикул:$attributes->product_code??'',
-            'product_name_new'=>(string)$xml_product->Наименование,
-            'product_description_new'=>(string)$xml_product->Описание,
+            'product_name'=>(string)$xml_product->Наименование,
+            'product_description'=>(string)$xml_product->Описание,
             'product_barcode'=>(string)$xml_product->Штрихкод,
             'product_unit'=>$this->productUnitGet($xml_product)
         ];
