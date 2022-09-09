8ee3e809476cdcb3f918eeb26b225ea6d2de66d7
bayci <bayci@BaycikNoteL>
2022-09-09 12:16:16 +0300

    bugfix

--- db_update/2022-08-06-1004.sql
+++ db_update/2022-08-06-1004.sql
@@ -1,7 +1,4 @@
 ALTER TABLE `transaction_list` 
-ADD COLUMN `trans_credit` VARCHAR(45) NULL AFTER `trans_debit`;
-
-ALTER TABLE `transaction_list` 
 ADD INDEX `tdebit` (`trans_debit` ASC),
 ADD INDEX `tcredit` (`trans_credit` ASC);
 ALTER TABLE `transaction_list` ALTER INDEX `thold`;
