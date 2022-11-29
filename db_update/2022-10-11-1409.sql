0e360cac4ab93c9781e61658942638d93dd8f9b1
bayci <bayci@BaycikNoteL>
2022-11-29 14:14:44 +0300

    new tariff system

--- db_update/2022-10-11-1409.sql
+++ db_update/2022-10-11-1409.sql
@@ -4,7 +4,7 @@ LIMIT 0, 50000
 
 -- Date: 2022-11-29 14:07
 */
-SET NAMES=utf8;
+
 INSERT INTO tariff_list (`tariff_id`,`tariff_name`,`order_allow`,`order_fee`,`order_cost`,`card_allow`,`card_fee`,`cash_allow`,`cash_fee`,`delivery_allow`,`delivery_fee`,`delivery_cost`,`is_disabled`,`is_public`,`created_at`,`updated_at`,`deleted_at`) VALUES (1,'Маркетплэйс',1,7,0,0,0,1,0,NULL,0,0,0,1,NULL,NULL,NULL);
 INSERT INTO tariff_list (`tariff_id`,`tariff_name`,`order_allow`,`order_fee`,`order_cost`,`card_allow`,`card_fee`,`cash_allow`,`cash_fee`,`delivery_allow`,`delivery_fee`,`delivery_cost`,`is_disabled`,`is_public`,`created_at`,`updated_at`,`deleted_at`) VALUES (7,'Маркетплэйс + оплата',1,7,0,1,3,0,0,NULL,0,0,0,0,NULL,NULL,NULL);
 INSERT INTO tariff_list (`tariff_id`,`tariff_name`,`order_allow`,`order_fee`,`order_cost`,`card_allow`,`card_fee`,`cash_allow`,`cash_fee`,`delivery_allow`,`delivery_fee`,`delivery_cost`,`is_disabled`,`is_public`,`created_at`,`updated_at`,`deleted_at`) VALUES (8,'Свобода (ресторан)',1,0,0,1,25,0,0,1,0,90,0,0,NULL,NULL,NULL);
