b46da94722e2c006fb77b44407c308fc679e17d9
bayci <bayci@BaycikNoteL>
2022-11-29 10:28:37 +0300

    new tariff system

--- db_update/2022-11-08-1407.sql
+++ db_update/2022-11-08-1407.sql
@@ -1,5 +1,7 @@
 ALTER TABLE `order_list` 
+CHANGE COLUMN `order_sum_total` `order_sum_total` FLOAT GENERATED ALWAYS AS (((`order_sum_product`+ `order_sum_delivery`) - `order_sum_promo`)) STORED ;
+
+ALTER TABLE `order_list` 
 DROP COLUMN `order_sum_tax`,
 DROP COLUMN `order_sum_fixed`,
-ADD COLUMN `order_data` JSON NULL AFTER `order_tariff`,
-CHANGE COLUMN `order_sum_total` `order_sum_total` FLOAT GENERATED ALWAYS AS (((`order_sum_product`+ `order_sum_delivery`) - `order_sum_promo`)) STORED ;
+ADD COLUMN `order_data` JSON NULL AFTER `order_tariff`;
