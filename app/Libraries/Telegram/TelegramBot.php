597e14a0c1144bd15bf67174b84c0f9cfae8755b
bayci <bayci@BaycikNoteL>
2022-12-23 15:23:35 +0300

    working on bot

--- app/Libraries/Telegram/TelegramBot.php
+++ app/Libraries/Telegram/TelegramBot.php
@@ -330,13 +330,13 @@ class TelegramBot{
     private function userPhoneRequest(){
         $this->userSignOut();
         $option=[
-            [$this->Telegram->buildKeyboardButton("✅✅✅ ВОЙТИ ✅✅✅",true)]
+            [$this->Telegram->buildKeyboardButton("⏩⏩⏩ НАЖМИ СЮДА ⏪⏪⏪",true)]
         ];
         $keyb = $this->Telegram->buildKeyBoard($option, $onetime=true);
         $content=[
             'chat_id'=>$this->Telegram->ChatID(),
             'reply_markup' => $keyb,
-            'text' => "Нажмите на поле внизу ✅✅✅ ВОЙТИ ✅✅✅ ниже, чтобы начать общение. Мне необходим ваш номер телефона учетной записи на https://tezkel.com",
+            'text' => "Нажмите на кнопку \"НАЖМИ СЮДА\" внизу, чтобы начать общение. Мне необходим ваш номер телефона учетной записи на https://tezkel.com",
             'disable_web_page_preview'=>1,
         ];
         $this->Telegram->sendMessage($content);
