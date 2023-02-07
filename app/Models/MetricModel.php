2d9800ccb4651f8c31847af84e65d66c3d9d951f
bayci <bayci@BaycikNoteL>
2023-02-07 15:42:45 +0300

    working on metrics

---
+++ app/Models/MetricModel.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class MetricModel extends Model{
+    
+    use FilterTrait;
+    
+    protected $table      = 'metric_list';
+    protected $primaryKey = 'metric_id';
+    protected $allowedFields = [
+        'come_referrer',
+        'come_url',
+        'come_media_id',
+        'come_inviter_id',
+        'user_id',
+        'device_is_mobile',
+        'device_platform'
+        ];
+
+    protected $useSoftDeletes = false;
+    protected $createdField=    'created_at';
+    protected $updatedField=    'updated_at';
+    
+    
+    public function itemGet(){
+        return false;
+    }
+    
+    public function itemCreate( $metric ){
+        return $this->insert($metric,true);
+    }
+    
+    public function itemUpdate(){
+        return false;
+    }
+    
+    public function itemDelete(){
+        return false;
+    }
+    
+    public function listGet(){
+        return false;
+    }
+    
+    public function listCreate(){
+        return false;
+    }
+    
+    public function listUpdate(){
+        return false;
+    }
+    
+    public function listDelete(){
+        return false;
+    }
+    
+}
