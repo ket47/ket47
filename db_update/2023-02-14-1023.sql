070707a9724691096306428b2a4dd32b2cec7fee
bayci <bayci@BaycikNoteL>
2023-02-19 19:46:04 +0300

    bugfix

--- db_update/2023-02-14-1023.sql
+++ db_update/2023-02-14-1023.sql
@@ -1,3 +1,1 @@
-ALTER TABLE `transaction_account_list` 
-ADD INDEX `transgtype` (`group_type` ASC);
-ALTER TABLE `transaction_account_list` ALTER INDEX `transaccunq`;
+ALTER TABLE `transaction_account_list` ADD INDEX `transgtype` (`group_type` ASC);
