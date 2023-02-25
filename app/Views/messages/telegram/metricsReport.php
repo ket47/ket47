c1d56b46bbed54b7385b84015855d0cd63e39f61
bayci <bayci@BaycikNoteL>
2023-02-25 15:30:13 +0300

    working on bot

--- app/Views/messages/telegram/metricsReport.php
+++ app/Views/messages/telegram/metricsReport.php
@@ -3,5 +3,5 @@ $i=0;
 ?><pre>
 <?php foreach($coming_list as $row): ?>
 <?php $url=parse_url($row->come_referrer);?>
-<?=++$i?>)📲<?=$row->media_name??'нет'?> 🌐<?=$url['host']??'нет'?> 
+<?=++$i?>)📲<?=$row->media_name??'-'?> <?=$url['host']?"🌐{$url['host']}":''?> <?=$row->device_platform=='iOS'?'🍏':''?><?=$row->device_platform=='Android'?'🤖':''?><?=$row->device_platform=='Windows'?'🪟':''?>
 <?php endforeach;?></pre>
