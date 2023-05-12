b1d3f7c0e51468594b6ed6c57ca234a86f049ec2
bayci <bayci@BaycikNoteL>
2023-05-12 14:18:46 +0300

    bugfix

--- app/Views/messages/order/on_delivery_search_COUR_sms.php
+++ app/Views/messages/order/on_delivery_search_COUR_sms.php
@@ -1,3 +1,5 @@
 <b>Задание <u>#<?=$order->order_id?></u></b> 
 Забрать из: <?=$order->store_name??$order->store->store_name?> 
+<?php if($order->location_longitude??''): ?>
 Адрес:<a href='https://yandex.ru/maps/?pt=<?=$order->location_longitude?>,<?=$order->location_latitude?>&z=19&l=map,trf'><?=$order->location_address?></a>
+<?php endif; ?>
