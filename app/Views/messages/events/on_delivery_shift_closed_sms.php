451846cece090b2a5b0dc15a187a4524f6200027
bayci <bayci@BaycikNoteL>
2023-07-18 14:36:11 +0300

    visual improvemets

--- app/Views/messages/events/on_delivery_shift_closed_sms.php
+++ app/Views/messages/events/on_delivery_shift_closed_sms.php
@@ -1,10 +1,10 @@
 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑
 <b><?=$courier->courier_name?></b>, ваша смена закрыта. 
 
-<pre>Начало:      </pre><b><?=$shift->created_at?></b>
-<pre>Конец:       </pre><b><?=$shift->finished_at?></b>
-<pre>Длительность:</pre><b><?=sprintf('%02d:%02d:%02d', ($total_duration??0)/3600,($total_duration??0)/60%60, ($total_duration??0)%60);?></b>
-<pre>Маршрут:     </pre><b><?=round(($total_distance??0)/1000)?>км</b>
+<pre>Начало  </pre><b><?=$shift->created_at?></b>
+<pre>Конец   </pre><b><?=$shift->finished_at?></b>
+<pre>Время   </pre><b><?=sprintf('%02d:%02d:%02d', ($total_duration??0)/3600,($total_duration??0)/60%60, ($total_duration??0)%60);?></b>
+<pre>Маршрут </pre><b><?=round(($total_distance??0)/1000)?>км</b>
 
 Задания не доступны. 
 Новый статус <u>ОТБОЙ</u> 💤
