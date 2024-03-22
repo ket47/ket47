5ca383e18485e13ae02bf9c6c2aa5828c2945c44
bayci <bayci@BaycikNoteL>
2024-03-22 15:08:08 +0300

    new courier motivation

--- app/Views/messages/events/on_delivery_shift_closed_sms.php
+++ app/Views/messages/events/on_delivery_shift_closed_sms.php
@@ -1,30 +1,39 @@
 <?php
-    $order_cost=150;
-    $hour_cost=170;
-    $hour_cost_car=190;
+    if( !function_exists('r50') ){
+        function r50( $num ){
+            return round($num/50)*50;
+        }  
+    }
 
-    $order_count_all=$statistics->order_count??0;
-    $order_count_heavy=$statistics->heavy_count??0;
-    $order_count_standart=$order_count_all-$order_count_heavy;
+    $order_cost     =70;
+    $hour_cost_rent =160;
+    $hour_cost_own  =190;
 
-    $order_sum_standart=(int)($order_cost*$order_count_all);
-    $order_sum_all=(int)($order_sum_standart+$statistics->heavy_bonus??0);
+    $heavy_bonus_moped  =$statistics->heavy_bonus??0;
+    $heavy_bonus_car    =0;
+    $order_count_all    =$statistics->order_count??0;
+    $order_sum_all      =r50($order_cost*$order_count_all);
+    $hour_sum_rent      =r50($total_duration*($hour_cost_rent/3600));
+    $hour_sum_own       =r50($total_duration*($hour_cost_own/3600));
 
-    $shift_sum_standart=round($total_duration*($hour_cost/3600)/50)*50;
-    $shift_sum_car=(int)($total_duration*($hour_cost_car/3600)/50)*50;
+    $total_moped_rent   =r50($hour_sum_rent+$order_sum_all+$heavy_bonus_moped);
+    $total_moped_own    =r50($hour_sum_own +$order_sum_all+$heavy_bonus_moped);
+    $total_car_own      =r50($hour_sum_own +$order_sum_all+$heavy_bonus_car);
 ?>
 🏁🏁🏁🏁🏁🏁🏁🏁🏁🏁
 <b><?=trim($courier->courier_name)?></b>, ваша смена закрыта. 
 
 <pre>Начало  <?=$shift->created_at?> </pre>
 <pre>Конец   <?=$shift->closed_at?> </pre>
-<pre>Время   <?=sprintf('%02d:%02d:%02d', ($total_duration??0)/3600,($total_duration??0)/60%60, ($total_duration??0)%60);?> 
-Мопед   <?=$shift_sum_standart?>₽ (+<?=$statistics->heavy_bonus??0?>₽) 
-Авто    <?=$shift_sum_car?>₽   
-</pre>
-<pre>Заказы  <?=$order_count_all?>шт 
-Оплата  <?=$order_sum_all?>₽ 
-</pre>
+<pre>Время   ⏱️<?=sprintf('%02d:%02d:%02d', ($total_duration??0)/3600,($total_duration??0)/60%60, ($total_duration??0)%60);?> 
+Заказы  📃<?=$order_count_all?>шт </pre>
+
+🏍️Мопед личный <b><?=$total_moped_own?>₽</b>
+<pre>⏱️<?=$hour_sum_own?>₽ 📃<?=$order_sum_all??0?>₽ ⛈️<?=$heavy_bonus_moped?>₽</pre> 
+🛵Мопед аренда <b><?=$total_moped_rent?>₽</b>
+<pre>⏱️<?=$hour_sum_rent?>₽ 📃<?=$order_sum_all??0?>₽ ⛈️<?=$heavy_bonus_moped?>₽</pre> 
+🚗Авто личное <b><?=$total_car_own?>₽</b>
+<pre>⏱️<?=$hour_sum_own?>₽ 📃<?=$order_sum_all??0?>₽ ⛈️<?=$heavy_bonus_car?>₽</pre>
 
 Задания не доступны. 
 Новый статус <u>ОТБОЙ</u> 💤
