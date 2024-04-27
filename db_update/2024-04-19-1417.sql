c7970b3aa17c22526c7e292bab54349d7abba2bd
bayci <bayci@BaycikNoteL>
2024-04-27 15:21:23 +0300

    bugfix

--- db_update/2024-04-19-1417.sql
+++ db_update/2024-04-19-1417.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `store_list` 
-CHANGE COLUMN `store_delivery_methods` `store_delivery_methods` VARCHAR(3000) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NULL DEFAULT NULL ;
+ADD COLUMN `store_delivery_methods` VARCHAR(3000) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NULL DEFAULT NULL ;
