ef6bc8127849b52d60cc2ae351d6efc6b4a5229b
bayci <bayci@BaycikNoteL>
2024-12-04 17:20:10 +0300

    visual improvements

--- app/Views/messages/mailing/email_basic.php
+++ app/Views/messages/mailing/email_basic.php
@@ -1,65 +1,77 @@
-<table style="width: 100%; height: 100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#F5F7FA">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title><?= $message_subject??'' ?></title>
+</head>
+<style>
+    html,body{
+        padding: 0px;
+        margin:0px;
+    }
+</style>
+<body>
+<table style="width: 100%; height: 100%;background-color: #F5F7FA;">
     <tbody>
         <tr>
             <td style="width: 100%" align="center">
-                <table style="width: 100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#F5F7FA">
+                <table style="width: 100%">
                     <tbody>
-                        <tr style="width: 100%; height: 30px">
+                        <tr style="height: 30px">
                             <td>&nbsp;</td>
                         </tr>
                         <tr>
-                            <td style="width: 100%">
-                                <table style="width: 100%" border="0" cellspacing="0" cellpadding="0">
+                            <td>
+                                <table style="width: 100%;">
                                     <tbody>
                                         <tr>
-                                            <td style="width: 5%" bgcolor="#F5F7FA">&nbsp;</td>
-                                            <td style="max-width: 420px" align="center">
-                                                <table style="width: 100%; max-width: 420px; border-radius: 8px; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; color: #0b1f33; box-shadow: rgba(0, 0, 0, 0.14) 0px 0px 15px -5px; margin: 10px;"
-                                                    border="0" cellspacing="0" cellpadding="0" bgcolor="#ffffff">
+                                            <td style="width: 5%">&nbsp;</td>
+                                            <td align="center">
+                                                <table style="width: 100%;max-width: 800px;background-color:white;border-spacing: 0px; border-radius: 8px; font-size: 16px; line-height: 24px; color: #0b1f33; box-shadow: rgba(0, 0, 0, 0.14) 0px 0px 15px -5px; margin: 10px;">
                                                     <tbody>
+                                                        <?php if( !empty($images->header) ): ?>
                                                         <tr>
-                                                            <td>
-                                                                <img src="cid:<?= $images->header ?>" 
-                                                                style="margin-right: 0px; width: 100%; border-radius: 8px 8px 0 0;"
-                                                                alt="logo_top.png" width="500"/>
+                                                            <td style="padding:0px">
+                                                                <img src="cid:<?= $images->header ?>" style="margin-right: 0px; width: 100%; border-radius: 8px 8px 0 0;"/>
                                                              </td>
                                                         </tr>
+                                                        <?php endif; ?>
+                                                        <?php if(!empty($message_subject)): ?>
                                                         <tr>
                                                             <td style="padding: 32px 30px 0px; font-size: 20px; line-height: 28px; height: 56px" align="center">
                                                                 <strong><?= $message_subject ?></strong>
                                                             </td>
                                                         </tr>
+                                                        <?php endif; ?>
                                                         <?php if(!empty($images->content)){ ?>
                                                         <tr>
                                                             <td style="padding: 0px 25px 0px; height: 96px" align="center">
                                                                 <p>
-                                                                    <img style="border-radius: 10px; width: 100%;"  src="cid:<?= $images->content ?>"  width="450">
+                                                                    <img style="border-radius: 10px; width: 100%;"  src="cid:<?= $images->content ?>">
                                                                 </p>
                                                             </td>
                                                         </tr>
                                                         <?php } ?>
                                                         <tr>
-                                                            <td style="padding: 12px 30px 0px; height: 48px" align="center">
+                                                            <td style="padding: 12px 30px 0px; height: 48px">
                                                                 <?= $message_text ?>
                                                             </td>
                                                         </tr>
                                                         <tr>
-                                                            <td style="padding: 12px 30px 0px;"
-                                                                align="center">С заботой о Вас, команда Tezkel.</td>
+                                                            <td style="padding:  30px 30px  30px;">С заботой о Вас, команда <?=getenv('app.title')?>.</td>
                                                         </tr>
                                                         
-                                                        <?php if(!empty($message_data->link)){ ?>
+                                                        <?php if(!empty($message_data->link)): ?>
                                                         <tr>
                                                             <td style="padding: 32px 30px;" align="center">
                                                                 <a style="text-decoration: none; background-color: #009dcd; padding: 10px 15px; border-radius: 10px; color: white; font-weight: bold"
                                                                     href="<?= $message_data->link ?>"
                                                                     target="_blank" rel="noreferrer">ПОДРОБНЕЕ</a></td>
                                                         </tr>
-                                                        <?php } ?>
+                                                        <?php endif; ?>
                                                     </tbody>
                                                 </table>
                                             </td>
-                                            <td style="width: 5%" bgcolor="#F5F7FA">&nbsp;</td>
+                                            <td style="width: 5%">&nbsp;</td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -70,22 +82,23 @@
             </td>
         </tr>
         <tr>
-            <td style="width: 100%" align="center">
-                <table style="width: 100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#F5F7FA">
+            <td>
+                <table style="width: 100%">
                     <tbody>
                         <tr>
-                            <td style="padding-bottom: 32px" align="center">
+                            <td style="padding-bottom: 32px">
                                 <table
                                     style="width: 100%; font-family: Arial, sans-serif; font-size: 14px; line-height: 20px; color: #86909c"
                                     border="0" cellspacing="0" cellpadding="0" bgcolor="transparent">
                                     <tbody>
                                         <tr>
-                                            <td style="padding: 32px 0px 12px; font-family: Arial, sans-serif; font-size: 14px; line-height: 20px; color: #86909c"
-                                                align="center"><span class="v1v1v1gmail-il">Скачать</span> мобильное
-                                                приложение <span class="v1v1v1LI v1v1v1ng">Tezkel</span></td>
+                                            <td  style="color: #86909c;text-align:center">
+                                                <span class="v1v1v1gmail-il">Скачать</span> мобильное приложение <?=getenv('app.title')?>
+                                            </td>
                                         </tr>
+                                        <?php if(!empty($images->buttonPM)): ?>
                                         <tr>
-                                            <td style="padding: 0px 20px 20px" align="center">
+                                            <td  style="color: #86909c;text-align:center">
                                                 <table style="width: 100%" border="0" cellspacing="0" cellpadding="0">
                                                     <tbody>
                                                         <tr>
@@ -104,10 +117,10 @@
                                                 </table>
                                             </td>
                                         </tr>
+                                        <?php endif; ?>
                                         <tr>
-                                            <td style="padding: 0px 8%; font-family: Arial, sans-serif; font-size: 14px; line-height: 20px; color: #86909c"
-                                                align="center">Вы получили это письмо, потому что зарегистрированы на
-                                                <span class="v1v1v1LI v1v1v1ng">Tezkel</span>.</td>
+                                            <td style="color: #86909c;text-align:center">Вы получили это письмо, потому что зарегистрированы на
+                                                <?=getenv('app.title')?>.</td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -119,3 +132,5 @@
         </tr>
     </tbody>
 </table>
+</body>
+</html>
--- app/Views/messages/order/on_customer_start_STORE_email.php
+++ app/Views/messages/order/on_customer_start_STORE_email.php
@@ -1,21 +1,8 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Заказ №<?= $order->order_id ?> от <?= getenv('app.title') ?></title>
-    </head>
-    <body>
-        <h2> Новый заказ №<?= $order->order_id ?></h2>
-
         <p>
             Добрый день, <?=$store->store_name?>.
         </p>
-        <p >
-            <a href="https://tezkel.com/order/order-<?= $order->order_id ?>">
-                новый заказ №<?= $order->order_id ?>.
-            </a>
-        </p>
         <p>
-            Мы получили заказ для вас. Поиск курьера уже запущен, но пока не найден. Когда мы найдем курьера вы будете уведомлены. Больше действий с заказом <a href="https://tezkel.com/order/order-<?= $order->order_id ?>">в приложении</a>
+            Мы получили заказ для вас. Больше действий с заказом <a href="https://tezkel.com/order/order-<?= $order->order_id ?>">в приложении</a>
         </p>
 
         <?php if ($order->order_description): ?>
@@ -32,38 +19,41 @@
         <?php endif; ?>
         <h3>Состав заказа</h3>
         <style>
-            table{
+            .order_table{
                 border-collapse: collapse;
                 border:1px solid #ccc;
                 width:100%;
             }
-            table td,table th{
+            .order_table td,table th{
                 padding:10px;
                 border-bottom:1px solid #ccc;
             }
-            table tr:nth-child(even){
+            .order_table tr:nth-child(even){
                 background-color: #f5fcff;
             }
         </style>
-        <table>
+        <table class="order_table">
             <tr style="border-bottom: 2px #6cf solid;">
                 <th></th>
                 <th>Товар</th>
                 <th style="text-align: right">Количество</th>
                 <th style="text-align: right">Цена</th>
                 <th style="text-align: right">Сумма</th>
-                <th>Комментрий</th>
             </tr>
                 <?php $active_count = 0;
                 foreach ($order->entries as $entry): ?>
                 <?php if ($entry->deleted_at) continue; ?>
                 <tr>
                     <td><?= ++$active_count ?></td>
-                    <td><?= $entry->entry_text ?></td>
+                    <td>
+                        <?= $entry->entry_text ?> 
+                        <?php if( !empty($entry->entry_comment) ): ?>
+                        [<?= $entry->entry_comment ?>]
+                        <?php endif; ?>
+                    </td>
                     <td style="text-align: right"><?= $entry->entry_quantity ?></td>
                     <td style="text-align: right"><?= $entry->entry_price ?></td>
                     <td style="text-align: right"><?= $entry->entry_sum ?></td>
-                    <td><?= $entry->entry_comment ?></td>
                 </tr>
                 <?php endforeach; ?>
             <tr>
@@ -71,8 +61,3 @@
                 <td style="text-align: right;font-weight: bold"><?= $order->order_sum_product ?></td>
             </tr>
         </table>
-        <p>
-            С уважением,  <?= getenv('app.title') ?> Bot.
-        </p>
-    </body>
-</html>
