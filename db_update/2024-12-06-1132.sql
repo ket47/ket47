05770ab669ba58f1d7b3cb3ad6b0612528db8e82
bayci <bayci@BaycikNoteL>
2024-12-06 12:32:54 +0300

    post update

--- db_update/2024-12-06-1132.sql
+++ db_update/2024-12-06-1132.sql
@@ -2,5 +2,5 @@ ALTER TABLE `post_list`
 ADD COLUMN `post_holder` VARCHAR(45) NULL AFTER `post_route`,
 ADD COLUMN `post_holder_id` INT NULL AFTER `post_holder`,
 CHANGE COLUMN `post_type` `post_type` ENUM('homeslide', 'wellcomeslide', 'story') NULL DEFAULT NULL ,
-ADD INDEX `pholder` (`post_holder` ASC, `post_holder_id` ASC) VISIBLE;
+ADD INDEX `pholder` (`post_holder` ASC, `post_holder_id` ASC);
 ;
