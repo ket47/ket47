220514ba233555b310f20d8973af311e5dd04111
bayci <bayci@BaycikNoteL>
2025-12-23 12:05:28 +0300

    bugfix

--- db_update/2025-12-10-1518.sql
+++ db_update/2025-12-10-1518.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `delivery_job_list` 
 ADD COLUMN `courier_name` VARCHAR(15) NULL AFTER `courier_id`,
-ADD COLUMN `courier_image_hash` VARCHAR(32) NULL AFTER `courier_name`
+ADD COLUMN `courier_image_hash` VARCHAR(32) NULL AFTER `courier_name`,
 ADD COLUMN `job_courier_type` ENUM('auto', 'shift', 'taxi') NULL DEFAULT 'auto'  AFTER `job_name`;
